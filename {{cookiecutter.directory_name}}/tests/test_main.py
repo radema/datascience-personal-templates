fa30b84733513f6d940d25455664da73ddc9d108
Raul De Maio <26902368+radema@users.noreply.github.com>
2025-05-07 22:40:09 +0200

    Update test_main.py

--- {{cookiecutter.directory_name}}/tests/test_main.py
+++ {{cookiecutter.directory_name}}/tests/test_main.py
@@ -1,8 +1,11 @@
 """Unit tests for the main module."""
+import sys
+
+sys.path.append("../src")  # Adjust the path to your src directory
 
 
 def test_main_runs():
-    """Test the main function in src/main.py."""
-    from src.main import main
+    """Test if the main function runs without errors."""
+    from main import main
 
     assert main() is None  # or your actual expected behavior
